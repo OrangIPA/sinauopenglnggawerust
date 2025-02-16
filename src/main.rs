f6adbc746e7005d945ea2b28813152f6069101e1
Bahana Alana Khansa <alanabahana102@gmail.com>
2025-02-16 21:47:51 +0700

    initial commit: chapter hello window

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use gl::COLOR_BUFFER_BIT;
+use glfw::{Action, Context};
+
+fn main() {
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    let (mut window, _events) = glfw
+        .create_window(800, 600, "LearnOpenGL", glfw::WindowMode::Windowed)
+        .unwrap();
+
+    gl::load_with(|s| window.get_proc_address(s) as *const std::os::raw::c_void);
+
+    window.make_current();
+    window.set_framebuffer_size_polling(true);
+
+    while !window.should_close() {
+        process_input(&mut window);
+
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(COLOR_BUFFER_BIT);
+        };
+
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_input(window: &mut glfw::Window) {
+    if window.get_key(glfw::Key::Escape) == Action::Press {
+        window.set_should_close(true);
+    }
+}
